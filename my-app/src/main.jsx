6f1987107245c8ae9902e70fbfc9bec90f9deb2f
coolfede97 <coolfede97@gmail.com>
2026-05-04 15:57:07 -0300

    first class

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import MiniProject from "./miniProject.jsx";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <div class="w-full h-full">
+      <MiniProject />
+    </div>
   </StrictMode>,
-)
+);
---
+++ my-app/src/miniProject.jsx
@@ -0,0 +1,9 @@
+function MiniProject() {
+  return (
+    <div class="bg-black w-1/4 h-1/5">
+      <img src="./src/assets/p.png" alt="pinguino"></img>
+    </div>
+  );
+}
+
+export default MiniProject;
