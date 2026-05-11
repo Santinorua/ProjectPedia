cbcf2594f9d19767f6ae95d157d7bde4bcd5c4bd
coolfede97 <coolfede97@gmail.com>
2026-05-11 17:17:14 -0300

    creo que miniproject component terminado

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -4,9 +4,27 @@ import "./index.css";
 import MiniProject from "./miniProject.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="flex flex-wrap w-full h-full">
-      <MiniProject />
-      <MiniProject />
+    <div className="flex flex-wrap justify-center gap-4 p-4 w-full h-full">
+      <MiniProject
+        title="Ruani enfermo de mrd"
+        description="Una descripción épica sobre geckos."
+        image="./src/assets/gecko.jpg"
+      />
+      <MiniProject
+        title="Ruani veni a labuar"
+        description="Una descripción épica sobre geckos."
+        image="./src/assets/p.png"
+      />
+      <MiniProject
+        title="Mira attack on titan al menos"
+        description="Una descripción épica sobre geckos."
+        image="./src/assets/gecko.jpg"
+      />
+      <MiniProject
+        title="Este si es un test serio"
+        description="deah, mentira."
+        image="./src/assets/p.png"
+      />
     </div>
   </StrictMode>,
 );
--- my-app/src/miniProject.jsx
+++ my-app/src/miniProject.jsx
@@ -1,14 +1,15 @@
-function MiniProject() {
+function MiniProject({ title, description, image }) {
   return (
-    <div class="flex flex-col items-center">
-      <img
-        className="bg-black w-1/4 h-1/5"
-        src="./src/assets/p.png"
-        alt="pinguino"
-      ></img>
-      <h1>Title</h1>
+    <div className="flex flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+      <div className="aspect-video w-full overflow-hidden rounded-lg bg-black">
+        <img className="w-full h-full object-cover" src={image} alt={title} />
+      </div>
+
+      <div className="mt-2">
+        <h1 className="text-white font-bold text-lg leading-tight">{title}</h1>
+        <p className="text-gray-400 text-sm">{description}</p>
+      </div>
     </div>
   );
 }
-
 export default MiniProject;
