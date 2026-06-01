83aab5ebf646180bd526ed47bd527544214edc60
Santinorua <santinoruani@gmail.com>
2026-05-31 23:35:08 -0300

    Final barra superior

--- my-app/src/barraBusqueda.jsx
+++ my-app/src/barraBusqueda.jsx
@@ -1,7 +1,7 @@
 
 function BarraBusqueda() {
     return (
-        <input type="text" className="bg-[var(--white)] w-1/2 h-1/2 outline-none rounded-full"/>
+        <input type="text" className="bg-[var(--white)] w-1/2 h-1/2 outline-none rounded-full pl-5" placeholder="Buscar"/>
     )
 }
 
--- my-app/src/barraSuperior.jsx
+++ my-app/src/barraSuperior.jsx
@@ -4,11 +4,13 @@ import BarraBusqueda from "./barraBusqueda.jsx"
 function BarraSuperior(){
     return (
         <div class="w-full h-[9%] bg-[var(--lightGray)] flex items-center">
-            <div class="w-1/3 h-full"></div>
-            <div class="w-1/2 h-full flex items-center justify-center">
+            <div class="w-1/6 h-full">
+                <img src="src/assets/logo.png" alt="" className="pl-4 h-full" />
+            </div>
+            <div class="w-3/4 h-full flex items-center justify-center">
                 <BarraBusqueda />
             </div>
-            <div class="w-1/5 h-full flex items-center gap-4">
+            <div class="w-1/6 h-full flex items-center gap-4 pr-4 justify-end">
                 <UserBarra logueado={false} />
             </div>
         </div>
